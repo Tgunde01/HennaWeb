c2e38d9ba735948c1b68441e106c6f774c62b989
TGunde01 <Tgunderman01@gmail.com>
2023-07-14 15:05:17 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = "Data Binding Examples"
+  firstName= "Jack";
+  lastName= "Martin";
+
+  isDisabled: boolean = true;
+
+constructor(){
+  setTimeout(()=>{
+    this.isDisabled = false;
+  },2000);
+}
+
+getName():string {
+  return this.firstName + ' ' + this.lastName;
+}
+getIsDisabled():boolean{
+  return this.isDisabled;
+}
+}
